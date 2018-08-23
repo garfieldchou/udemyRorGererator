50d7c6eb414b2801cfddf9dda7211b347da9e5e4
garfield <cmgarfield@gmail.com>
2018-08-23 09:22:01 +0800

    $ rails db:migrate for Guilde scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_010956) do
+ActiveRecord::Schema.define(version: 2018_08_23_012012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "guides", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
